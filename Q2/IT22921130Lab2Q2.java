985bcbe063b05cd4352b88691ecf788909f13f05
IT22921130 <127285898+IT22921130@users.noreply.github.com>
2026-08-28 20:40:31 +0530

    Add IT22921130.java to Lab02Q2

---
+++ Q2/IT22921130Lab2Q2.java
@@ -0,0 +1,13 @@
+public class IT22921130Lab2Q2 {
+    public static void main(String[] args) {
+
+        double side = 10;
+        double pi = 3.14;
+
+        double ropeLength = 4 * side;
+
+        double radius = ropeLength / (2 * pi);
+
+        System.out.println("Radius = " + radius);
+    }
+}
